8ddd510be8c01df609e8725adcccccb2c8ed8bee
Johann Wagner <johann.wagner@wobcom.de>
2023-09-14 07:52:30 +0200

    Implemented log drain

--- src/lib/vicky/errors.rs
+++ src/lib/vicky/errors.rs
@@ -1,6 +1,8 @@
+use aws_sdk_s3::{operation::{upload_part::UploadPartError, complete_multipart_upload::CompleteMultipartUploadError, put_object::PutObjectError, get_object::GetObjectError}, primitives::ByteStreamError};
 use log::error;
 use rocket::{response::Responder, Request, http::Status};
 use thiserror::Error;
+use tokio::sync::broadcast::error::SendError;
 
 #[derive(Error, Debug)]
 pub enum VickyError {
@@ -23,27 +25,67 @@ pub enum VickyError {
         #[from] source: uuid::Error,
     },
 
-    #[error("HTTP Error {source:?}")]
-    Http {
-        #[from] source: HTTPError,
-    },
+    #[error("HTTP Error {0:?}")]
+    HttpError(Status),
 
     #[error("Scheduling Error {source:?}")]
     Scheduler {
         #[from] source: SchedulerError,
-    }
-}
+    },
 
-#[derive(Error, Debug)]
-pub enum HTTPError {
-    #[error("Resource Not Found")]
-    NotFound
+    #[error("Push Error {source:?}")]
+    PushError {
+        #[from] source: SendError<(String, String)>,
+    },
+
+    #[error("S3 Client Error {source:?}")]
+    S3ClientError {
+        #[from] source: S3ClientError,
+    }
 }
 
 #[derive(Error, Debug)]
 pub enum SchedulerError {
     #[error("Invalid Scheduling")]
     GeneralSchedulingError
+}
+
+
+#[derive(Error, Debug)]
+pub enum S3ClientError {
+    #[error("Object Already Exists")]
+    ObjectAlreadyExistsError,
+
+    #[error("SDK Error {source:?}")]
+    SdkError {
+        #[from] source: aws_sdk_s3::Error,
+    },
+
+    #[error("SDK Error {source:?}")]
+    SdkError2 {
+        #[from] source: aws_sdk_s3::error::SdkError<UploadPartError>,
+    },
+
+    #[error("SDK Error {source:?}")]
+    SdkError3 {
+        #[from] source: aws_sdk_s3::error::SdkError<CompleteMultipartUploadError>,
+    },
+
+    #[error("SDK Error {source:?}")]
+    SdkError5 {
+        #[from] source: aws_sdk_s3::error::SdkError<PutObjectError>,
+    },
+
+    #[error("SDK Error {source:?}")]
+    SdkError6 {
+        #[from] source: aws_sdk_s3::error::SdkError<GetObjectError>,
+    },
+
+    #[error("SDK Error {source:?}")]
+    SdkError4 {
+        #[from] source: ByteStreamError,
+    }
+
 }
 
 
