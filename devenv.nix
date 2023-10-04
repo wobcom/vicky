4ba280e7ae90c90ebb8101cde8ab28406aec7934
Yureka <yuka@yuka.dev>
2023-10-04 20:43:59 +0200

    devenv: fix bucket name

--- devenv.nix
+++ devenv.nix
@@ -2,9 +2,10 @@
 
 {
   services.minio.enable = true;
-  services.minio.buckets = [ "testbucket" ];
+  services.minio.buckets = [ "vicky-logs" ];
 
   env.ETCD_DATA_DIR = config.env.DEVENV_STATE + "/etcd";
+
   processes = {
     etcd.exec = "${pkgs.etcd}/bin/etcd --listen-client-urls=http://127.0.0.1:2379 --advertise-client-urls=http://127.0.0.1:2379";
   };
