0ebb48ea46bf2fd7b70013ce19c5bc8041c94c63
Yureka <yuka@yuka.dev>
2023-10-04 18:56:50 +0200

    devenv: etcd state dir

--- devenv.nix
+++ devenv.nix
@@ -1,9 +1,10 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   services.minio.enable = true;
   services.minio.buckets = [ "testbucket" ];
 
+  env.ETCD_DATA_DIR = config.env.DEVENV_STATE + "/etcd";
   processes = {
     etcd.exec = "${pkgs.etcd}/bin/etcd --listen-client-urls=http://127.0.0.1:2379 --advertise-client-urls=http://127.0.0.1:2379";
   };
