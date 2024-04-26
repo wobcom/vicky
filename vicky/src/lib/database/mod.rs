78205fca0273642a4e5bab1b90212581626e290b
Kek5chen <kx@imkx.dev>
2024-04-26 10:36:31 +0200

    vickylib: added database schema

--- vicky/src/lib/database/mod.rs
+++ vicky/src/lib/database/mod.rs
@@ -1,3 +1,5 @@
+mod schema;
+
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 
---
+++ vicky/src/lib/database/schema.rs
@@ -0,0 +1,28 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    locks (id) {
+        id -> Int4,
+        task_id -> Nullable<Uuid>,
+        name -> Varchar,
+        #[sql_name = "type"]
+        type_ -> Varchar,
+    }
+}
+
+diesel::table! {
+    tasks (id) {
+        id -> Uuid,
+        display_name -> Nullable<Varchar>,
+        status -> Nullable<Varchar>,
+        flake_ref_uri -> Nullable<Varchar>,
+        args -> Nullable<Varchar>,
+    }
+}
+
+diesel::joinable!(locks -> tasks (task_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    locks,
+    tasks,
+);
