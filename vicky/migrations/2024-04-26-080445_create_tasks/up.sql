aad13dfe8382d1bc3e7ba1aa92d286756fd6cf28
Kek5chen <kx@imkx.dev>
2024-04-26 10:27:19 +0200

    vicky: added migrations and schema

---
+++ vicky/migrations/2024-04-26-080445_create_tasks/up.sql
@@ -0,0 +1,20 @@
+CREATE TABLE tasks
+(
+    id            uuid PRIMARY KEY,
+    display_name  VARCHAR,
+    status        VARCHAR,
+    flake_ref_uri VARCHAR,
+    args          VARCHAR
+);
+
+CREATE TABLE locks
+(
+    id      INT,
+    task_id uuid,
+    name    VARCHAR NOT NULL,
+    type    VARCHAR NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_task
+        FOREIGN KEY (task_id)
+            REFERENCES tasks (id)
+);
