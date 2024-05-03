a71e5677b784d7d62a4340d0d52b669dc0b33e95
KX <kx@imkx.dev>
2024-05-03 14:11:52 +0200

    added prepare_client to get a http client setup for vicky

---
+++ vickyctl/src/http_client.rs
@@ -0,0 +1,16 @@
+use std::error::Error;
+use reqwest::blocking::Client;
+use reqwest::header::{AUTHORIZATION, HeaderMap};
+use crate::AppContext;
+
+pub fn prepare_client(ctx: &AppContext) -> Result<Client, Box<dyn Error>> {
+    let mut default_headers = HeaderMap::new();
+    default_headers.insert(AUTHORIZATION, ctx.vicky_token.parse().unwrap());
+    let client = Client::builder()
+        .default_headers(default_headers)
+        // TODO?: .https_only(true)
+        .user_agent(format!("VickyCTL/{}", env!("CARGO_PKG_VERSION")))
+        .build()
+        .expect("HTTP Client could not be built");
+    Ok(client)
+}
