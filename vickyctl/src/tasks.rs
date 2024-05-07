068399d5df994d5a08f59793208af0a63686aab8
KX <kx@imkx.dev>
2024-05-07 14:28:57 +0200

    vickyctl: added TaskData::to_json

--- vickyctl/src/tasks.rs
+++ vickyctl/src/tasks.rs
@@ -17,3 +17,28 @@ pub fn show_tasks(tasks_args: &TasksArgs) -> Result<(), Box<dyn Error>> {
     handle_user_response(&tasks_args.ctx, &text)?;
     Ok(())
 }
+
+impl TaskData {
+    fn to_json(&self) -> serde_json::Value {
+        let locks: serde_json::Value = self
+            .lock_name
+            .iter()
+            .zip(self.lock_type.iter())
+            .map(|(name, ty)| {
+                json!({
+                    "name": name,
+                    "type": ty
+                })
+            })
+            .collect();
+        json!({
+            "display_name": self.name,
+            "flake_ref": {
+                "flake": self.flake_url,
+                "args": self.flake_arg
+            },
+            "locks": locks,
+            "features": self.features
+        })
+    }
+}
