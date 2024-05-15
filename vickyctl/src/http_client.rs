96e9484ed9a1bc09cc67db5d8193c661dfee1fe7
Kek5chen <kx@imkx.dev>
2024-05-15 10:05:40 +0200

    vickyctl: remove unnecessaary todo

--- vickyctl/src/http_client.rs
+++ vickyctl/src/http_client.rs
@@ -8,7 +8,6 @@ pub fn prepare_client(ctx: &AppContext) -> Result<Client, Error> {
     default_headers.insert(AUTHORIZATION, ctx.vicky_token.parse().unwrap());
     let client = Client::builder()
         .default_headers(default_headers)
-        // TODO?: .https_only(true)
         .user_agent(format!("vickyctl/{}", env!("CARGO_PKG_VERSION")))
         .build()?;
     Ok(client)
