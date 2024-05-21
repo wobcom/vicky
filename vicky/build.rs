0fa54c85faeeaa497c09e9897b8d8d93cb790cd7
Kek5chen <kx@imkx.dev>
2024-05-21 10:30:48 +0200

    vicky: use regex and make build script more versatile

--- vicky/build.rs
+++ vicky/build.rs
@@ -1,6 +1,8 @@
+use std::{fs, io};
 use std::fs::File;
-use std::io;
-use std::io::{BufRead, BufReader, BufWriter, ErrorKind, Write};
+use std::io::{BufWriter, Write};
+
+use regex::{Captures, Regex};
 
 fn main() {
     if let Err(e) = ensure_locks_id_nullable() {
@@ -8,41 +10,34 @@ fn main() {
     }
 }
 
+const SCHEMA_RS_PATH: &str = "src/lib/database/schema.rs";
+
+// tablename, fieldname, old type
+// add more optional table values here as needed
+const TABLE_FIELDS: [(&str, &str, &str); 1] = [("locks", "id", "Uuid")];
+
 fn ensure_locks_id_nullable() -> io::Result<()> {
     println!("cargo::rerun-if-changed=src/lib/database/schema.rs");
-    let schema_rs_path = "src/lib/database/schema.rs";
+    let mut new_schema = fs::read_to_string(SCHEMA_RS_PATH)?;
+    for (table_name, field_name, old_type) in TABLE_FIELDS.iter() {
+        let regex = Regex::new(&format!(
+            r"(diesel::table!\s*\{{\n\s*{}\s*\({}\)\s*\{{\n\s*{}\s*->\s*){}",
+            table_name, field_name, field_name, old_type,
+        ))
+        .expect("The values entered for the replacement were not valid.");
 
-    let schema = File::open(schema_rs_path)?;
-    let reader = BufReader::new(schema);
-    let mut new_schema: String = String::new();
-    let mut found_locks_table = false;
-    let mut only_copy = false;
-
-    for line in reader.lines() {
-        let line = line?;
-
-        if only_copy {
-            new_schema.push_str(&line);
-        } else if !found_locks_table {
-            if line.contains("locks (id) {") {
-                found_locks_table = true;
-            }
-            new_schema.push_str(&line);
-        } else {
-            if !line.contains("id ->") {
-                return Err(io::Error::new(
-                    ErrorKind::InvalidData,
-                    "The line following the table header should've contained the id primary key",
-                ));
-            } else if line.contains("id -> Nullable<Uuid>,") {
-                return Ok(()); // already corrected
-            }
-            new_schema.push_str(&line.replace(" Uuid,", " Nullable<Uuid>,"));
-            only_copy = true;
-        }
-        new_schema.push('\n');
+        new_schema = regex
+            .replace(&new_schema, |caps: &Captures| {
+                if let Some(before) = caps.get(1) {
+                    format!("{}Nullable<{}>", before.as_str(), old_type)
+                } else {
+                    caps.get(0).unwrap().as_str().to_string() // shouldn't get here, but just in case
+                }
+            })
+            .to_string();
     }
-    let schema = File::create(schema_rs_path)?;
+
+    let schema = File::create(SCHEMA_RS_PATH)?;
     let mut overwriter = BufWriter::new(schema);
     overwriter.write_all(new_schema.as_bytes())?;
     Ok(())
