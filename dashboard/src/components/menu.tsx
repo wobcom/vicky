6662fbdb0a9e199be1ad55a6cf7ccb1b657f6899
willow <52585984+Kek5chen@users.noreply.github.com>
2026-01-09 14:00:15 +0100

    fix: react v6 navbar

--- dashboard/src/components/menu.tsx
+++ dashboard/src/components/menu.tsx
@@ -1,44 +1,48 @@
-import { Nav, Navbar } from "rsuite"
+import {Avatar, Nav, Navbar} from "rsuite"
 
 import * as s from "./menu.module.css";
 
 import UserIcon from '@rsuite/icons/legacy/User';
 
-import { Link, useNavigate } from "react-router-dom";
-import { useContext, useEffect, useState } from "react";
-import { useAPI } from "../services/api";
-import { UserContext } from "../contexts/user";
-import { useAuth } from "react-oidc-context";
+import {Link, useNavigate} from "react-router-dom";
+import {useContext, useEffect, useState} from "react";
+import {useAPI} from "../services/api";
+import {UserContext} from "../contexts/user";
+import {useAuth} from "react-oidc-context";
+import AvatarIcon from "rsuite/esm/Avatar/AvatarIcon";
 
 
 const Menu = () => {
 
-    const activeKey = "tasks";
     const user = useContext(UserContext)
     const auth = useAuth();
 
     return (
-        <Navbar bordered shaded>
-            <Navbar.Brand>
-                <Link to={"/"}>Vicky</Link>
-            </Navbar.Brand>
-            {
-                user ? (
-                    <Nav activeKey={activeKey}>
-                        <Nav.Item eventKey="tasks" >
-                            <Link to={"/tasks"}>Tasks</Link>
+        <Navbar>
+            <Navbar.Content>
+                <Navbar.Brand>
+                    <Link to={"/"}>Vicky</Link>
+                </Navbar.Brand>
+                {
+                    user ? (
+                                <Nav>
+                                    <Nav.Item eventKey="tasks">
+                                        <Link to={"/tasks"}>Tasks</Link>
+                                    </Nav.Item>
+                                </Nav>
+                        )
+                        : null
+                }
+            </Navbar.Content>
+            <Navbar.Content>
+                <Nav>
+                    {user ? (
+                        <Nav.Item icon={<Avatar circle background={"transparent"} size="sm" />}>
+                            {user.full_name}
                         </Nav.Item>
-                    </Nav>
-                )
-                : null
-            }
-            <Nav pullRight>
-                { user ? (
-                    <Nav.Item icon={<UserIcon />}>
-                        {user.full_name}
-                    </Nav.Item>
-                ) : null}
-            </Nav>
+                    ) : null}
+                </Nav>
+            </Navbar.Content>
         </Navbar>
     )
 
