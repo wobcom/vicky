6789949b8133ac47f5bab30f054081625f7619cf
Yureka <yuka@yuka.dev>
2026-03-23 19:02:51 +0100

    wip: update nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "vicky";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }: {
